75e985d0c6ff167d9a37c4c318206feaa071c6ef
Tércio Souza de Jesus <terciodejesus@gmail.com>
2025-03-08 22:38:39 -0300

    feat: create upload file service

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
+const upload = multer()
 
 var app = express();
 
@@ -10,6 +12,22 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  try {
+    const originalName = req.file.originalname
+    const type = req.file.mimetype
+    const size = req.file.size
+
+    res.json({
+      name: originalName,
+      type,
+      size
+    })
+  } catch (err) {
+    res.json({error: err.message})
+  }
+})
 
 
 
